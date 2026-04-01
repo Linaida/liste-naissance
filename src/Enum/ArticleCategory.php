656d99fbc0d016abd00342b215b7ed326ca5db9d
linaida <linaida66@gmail.com>
2026-04-01 21:50:15 +0200

    refactor: Réorganiser les cas de l'énumération ArticleCategory pour une meilleure lisibilité

--- src/Enum/ArticleCategory.php
+++ src/Enum/ArticleCategory.php
@@ -4,26 +4,26 @@ namespace App\Enum;
 
 enum ArticleCategory: string
 {
-    case TOY = 'toy';
-    case FURNITURE = 'furniture';
-    case CLOTHES = 'clothes';
-    case HYGIENE = 'hygiene';
-    case TRANSPORT = 'transport';
     case FOOD = 'food';
+    case HYGIENE = 'hygiene';
+    case TOY = 'toy';
     case BED = 'bed';
+    case FURNITURE = 'furniture';
+    case TRANSPORT = 'transport';
+    case CLOTHES = 'clothes';
     case OTHER = 'other';
 
     public function label(): string
     {
         return match($this) {
-            self::TOY => 'Jouet',
-            self::FURNITURE => 'Mobilier',
-            self::CLOTHES => 'Vêtements',
-            self::HYGIENE => 'Hygiène',
-            self::TRANSPORT => 'Transport',
             self::FOOD => 'Alimentation',
+            self::HYGIENE => 'Hygiène',
+            self::TOY => 'Jouet',            
             self::BED => 'Literie',
-            self::OTHER => 'Autre',
+            self::FURNITURE => 'Mobilier',
+            self::TRANSPORT => 'Transport',
+            self::CLOTHES => 'Vêtements',
+            self::OTHER => '--- Autre ---',
         };
     }
 }
